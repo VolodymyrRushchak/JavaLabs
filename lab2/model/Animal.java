ca87013e026dd7ff9212f2f080e48a4792b55c73
VolodymyrRushchak <93164951+VolodymyrRushchak@users.noreply.github.com>
2022-04-05 01:51:48 +0300

    Add source files for lab 2

---
+++ lab2/model/Animal.java
@@ -0,0 +1,20 @@
+package lab2.model;
+
+public abstract class Animal {
+    private final boolean isCarnivorous;
+    private final double foodConsumption;   // in kilograms per day
+
+    public Animal(boolean isCarnivorous, double foodConsumption) {
+        this.isCarnivorous = isCarnivorous;
+        this.foodConsumption = foodConsumption;
+    }
+
+    public boolean isCarnivorous(){
+        return isCarnivorous;
+    }
+
+    public double getFoodConsumption(){
+        return foodConsumption;
+    }
+
+}
---
+++ lab2/model/SeaLion.java
@@ -0,0 +1,39 @@
+package lab2.model;
+
+public class SeaLion extends Animal {
+    private String name;
+    private String color;
+    private boolean eatsPenguins;
+
+    public SeaLion(double foodConsumption, String name, String color, boolean eatsPenguins) {
+        super(true, foodConsumption);
+        this.name = name;
+        this.color = color;
+        this.eatsPenguins = eatsPenguins;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean isEatsPenguins() {
+        return eatsPenguins;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setEatsPenguins(boolean eatsPenguins) {
+        this.eatsPenguins = eatsPenguins;
+    }
+
+}

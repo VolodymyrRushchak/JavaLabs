010f422e1925dcc1564b8460016349ef32b93329
VolodymyrRushchak <93164951+VolodymyrRushchak@users.noreply.github.com>
2022-04-18 16:32:49 +0300

    Fixing the project's structure

--- src/main/java/ua/lviv/iot/lab2/model/Seal.java
+++ src/main/java/ua/lviv/iot/lab2/model/Seal.java
@@ -1,25 +1,13 @@
 package ua.lviv.iot.lab2.model;
 
 public class Seal extends Animal {
-    private String name;
-    private String color;
     private boolean isCute;
     private boolean isRussian;
 
     public Seal(double foodConsumption, String name, String color, boolean isCute, boolean isRussian) {
-        super(true, foodConsumption);
-        this.name = name;
-        this.color = color;
+        super(true, foodConsumption, name, color);
         this.isCute = isCute;
         this.isRussian = isRussian;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getColor() {
-        return color;
     }
 
     public boolean isCute() {
@@ -28,14 +16,6 @@ public class Seal extends Animal {
 
     public boolean isRussian() {
         return isRussian;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
     }
 
     public void setCute(boolean cute) {

1602b34dd7f7083d8c6d76fa2155e93f162f7f13
VolodymyrRushchak <93164951+VolodymyrRushchak@users.noreply.github.com>
2022-05-04 04:09:30 +0300

    Update Seal.java

--- src/main/java/ua/lviv/iot/lab2/model/Seal.java
+++ src/main/java/ua/lviv/iot/lab2/model/Seal.java
@@ -1,5 +1,9 @@
 package ua.lviv.iot.lab2.model;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Seal extends Animal {
     private boolean isCute;
     private boolean isRussian;
@@ -8,22 +12,6 @@ public class Seal extends Animal {
         super(true, foodConsumption, name, color);
         this.isCute = isCute;
         this.isRussian = isRussian;
-    }
-
-    public boolean isCute() {
-        return isCute;
-    }
-
-    public boolean isRussian() {
-        return isRussian;
-    }
-
-    public void setCute(boolean cute) {
-        isCute = cute;
-    }
-
-    public void setRussian(boolean russian) {
-        isRussian = russian;
     }
 
 }

663185989d5afc810d0bb6ebf77ff0340ad5fdd2
VolodymyrRushchak <93164951+VolodymyrRushchak@users.noreply.github.com>
2022-05-04 04:09:03 +0300

    Update SeaLion.java

--- src/main/java/ua/lviv/iot/lab2/model/SeaLion.java
+++ src/main/java/ua/lviv/iot/lab2/model/SeaLion.java
@@ -1,18 +1,14 @@
 package ua.lviv.iot.lab2.model;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class SeaLion extends Animal {
     private boolean eatsPenguins;
 
     public SeaLion(double foodConsumption, String name, String color, boolean eatsPenguins) {
         super(true, foodConsumption, name, color);
-        this.eatsPenguins = eatsPenguins;
-    }
-
-    public boolean isEatsPenguins() {
-        return eatsPenguins;
-    }
-
-    public void setEatsPenguins(boolean eatsPenguins) {
         this.eatsPenguins = eatsPenguins;
     }
 
